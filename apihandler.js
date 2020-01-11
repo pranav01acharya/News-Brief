eda2ff1fea4818e85bf0f96fbf7bd5496ac2a625
Mingjia Wang <mingjiawangdev@gmail.com>
2020-01-11 10:58:31 -0800

    Added primitive code for handling apis

--- apihandler.js
+++ apihandler.js
@@ -1,1 +1,26 @@
-alert('the js file is working');
+let key = "XO1cYKAtTKHJ222AQn41KygfqgmHMBjT";
+var request = new XMLHttpRequest();
+
+function buildURL(section) {
+
+	return "https://api.nytimes.com/svc/topstories/v2/" + section + ".json?api-key=" + key;
+}
+
+function printData(data) {
+
+
+}
+request.open('GET', buildURL("science"), true);
+request.onload = function() {
+
+	var data = JSON.parse(this.response);
+
+	if (request.status >= 200 && request.status <= 400) {
+		//printData(data["results"]);
+		console.log(data);
+	} else {
+		console.log('error');
+	}
+}
+
+request.send()
